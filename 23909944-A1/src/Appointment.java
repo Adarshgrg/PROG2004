4f679bf63fc182baa9a87f9a67683f66ef4ad49f
Adarshgrg <adarshgurung10@gmail.com>
2024-11-13 23:47:55 +1100

    Appointment class has been added so that patient can make an appointment  and to store their basic details,time slot and type of doctor
    Variables for patient details have been initialized with their name, mobile number, time slot and type of doctor.
    The default and second constructor with parameters has been created with the instance variables. A print method has been created to print all the instance variables.
    
    Co-Authored-By: Adarsh Pandit <913757+adarsh@users.noreply.github.com>

---
+++ 23909944-A1/src/Appointment.java
@@ -0,0 +1,36 @@
+public class Appointment {
+    //Instance variables for patient details, time slot and selected doctor
+    private String patientName;
+    private String patientMobile;
+    private String timeSlot;
+    private HealthProfessional doctor; //can be specialist or GeneralPractitioner
+
+    //Default constructor
+    public Appointment()
+    {
+        this.patientName = "";
+        this.patientMobile = "";
+        this.timeSlot = "";
+        this.doctor = null; //no doctor selected by default
+
+    }
+    //Constructor to intialize all the instance variablers
+    public Appointment(String patientName, String patientMobile, String timeSlot, HealthProfessional doctor){
+        this.patientName = patientName;
+        this.patientMobile = patientMobile;
+        this.timeSlot = timeSlot;
+        this.doctor = doctor;
+
+    }
+    //Method to print the appointment details
+    public void printDetails(){
+
+        System.out.println("Appointment Details:");
+        System.out.println("Patient Name: " + patientName);
+        System.out.println("Patient Mobile: " + patientMobile);
+        System.out.println("Preferred Time Slot: " + timeSlot);
+        System.out.println("Selected Doctor: ");
+        doctor.printDetails();  // Calls the doctor's printDetails method (from HealthProfessional or its subclasses)
+        System.out.println("------------------------------");
+    }
+}
