1f36edb3466e796bb72b51df4b51b5f7e7ae8395
Adarshgrg <adarshgurung10@gmail.com>
2024-11-13 23:08:49 +1100

    Update AssignmentOne.java
    Three objects of General Practitioner have been created and two specialist objects have been created too.
    Methods to print have been used to print the details of the Health professional with their information.
    System.out.println("------------------------------"); code has been added.
    
    Co-Authored-By: Adarsh Pandit <913757+adarsh@users.noreply.github.com>

--- 23909944-A1/src/AssignmentOne.java
+++ 23909944-A1/src/AssignmentOne.java
@@ -1,17 +1,36 @@
 public class AssignmentOne {
     
     public static void main(String[] args){
+        //Part 3 Using classes and object
         
-   // Create a GeneralPractitioner object
-   GeneralPractitioner gp = new GeneralPractitioner(1, "Dr. Ram", "General", 4);
-   gp.printDetails();  // Print details of the General Practitioner
+        //Creating three objects of General Practitioner
+        
+        GeneralPractitioner gp1 = new GeneralPractitioner(1, "Dr. Ram", "General", 4);
+        GeneralPractitioner gp2 = new GeneralPractitioner(2, "Dr. Rimal", "General", 10);
+        GeneralPractitioner gp3 = new GeneralPractitioner(3, "Dr. Bhat", "General", 8);
 
-   System.out.println();  // Blank line between outputs
-   
-   // Create a Specialist object
-   Specialist specialist = new Specialist(2, "Dr. Johnson", "Specialist", "Physiotherapist");
-   specialist.printDetails();  // Print details of the Specialist
+
+        // Creating two objects of Specialist
+        Specialist specialist1 = new Specialist(4, "Dr.  Retard", "Specialist", "Cardiology");
+        Specialist specialist2 = new Specialist(5, "Dr.  Zone", "Specialist", "Physiotherapist");
+
+        // Printing the details of all health professionals
+        gp1.printDetails();  // Print details of the first General Practitioner
+        System.out.println("------------------------------");
+
+        gp2.printDetails();  // Print details of the second General Practitioner
+        System.out.println("------------------------------");
+
+        gp3.printDetails();  // Print details of the third General Practitioner
+        System.out.println("------------------------------");
+
+        specialist1.printDetails();  // Print details of the first Specialist
+        System.out.println("------------------------------");
+
+        specialist2.printDetails();  // Print details of the second Specialist
+        System.out.println("------------------------------");
+    }
 
    
     }
-}
+
